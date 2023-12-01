5c60e61f6d03d324da23d57ec0f85a7aa75ccfae
Alex Ira <27619740+4lexir4@users.noreply.github.com>
2023-12-01 12:16:22 +1100

    check limit

--- orderbook.go
+++ orderbook.go
@@ -5,6 +5,13 @@ import (
 	"time"
 )
 
+type Match struct {
+	Ask        *Order
+	Bid        *Order
+	SizeFilled float64
+	Price      float64
+}
+
 type Order struct {
 	Size      float64
 	Bid       bool
@@ -37,6 +44,10 @@ func NewLimit(price float64) *Limit {
 	}
 }
 
+func (l *Limit) String() string {
+	return fmt.Sprintf("[price: %.2f | total volume: %.2f]", l.Price, l.TotalVolume)
+}
+
 func (l *Limit) AddOrder(o *Order) {
 	o.Limit = l
 	l.Orders = append(l.Orders, o)
@@ -60,4 +71,51 @@ func (l *Limit) DeleteOrder(o *Order) {
 type Orderbook struct {
 	Asks []*Limit
 	Bids []*Limit
+
+	AskLimits map[float64]*Limit
+	BidLimits map[float64]*Limit
+}
+
+func NewOrderbook() *Orderbook {
+	return &Orderbook{
+		Asks:      []*Limit{},
+		Bids:      []*Limit{},
+		AskLimits: make(map[float64]*Limit),
+		BidLimits: make(map[float64]*Limit),
+	}
+}
+
+func (ob *Orderbook) PlaceOrder(price float64, o *Order) []Match {
+	// TODO: try to match the order
+
+	// add the rest of the order to the books
+	if o.Size > 0.0 {
+		ob.add(price, o)
+	}
+
+	return []Match{}
+
+}
+
+func (ob *Orderbook) add(price float64, o *Order) {
+	var limit *Limit
+
+	if o.Bid {
+		limit = ob.BidLimits[price]
+	} else {
+		limit = ob.AskLimits[price]
+	}
+
+	if limit == nil {
+		limit = NewLimit(price)
+		limit.AddOrder(o)
+
+		if o.Bid {
+			ob.Bids = append(ob.Bids, limit)
+			ob.BidLimits[price] = limit
+		} else {
+			ob.Asks = append(ob.Asks, limit)
+			ob.AskLimits[price] = limit
+		}
+	}
 }
