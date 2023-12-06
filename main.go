1a87dcf6af7f1eeb2ef391481dde58f1ac2a6308
Alex Ira <27619740+4lexir4@users.noreply.github.com>
2023-12-06 15:21:07 +1100

    order placed

--- main.go
+++ main.go
@@ -1,17 +1,67 @@
 package main
 
 import (
+	"encoding/json"
+
+	"github.com/4lexir4/cx/orderbook"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+	ex := NewExchange()
 
-	e.POST("/order", handlePlaceOrder)
+	e.POST("/order", ex.handlePlaceOrder)
 
 	e.Start(":3000")
 }
 
-func handlePlaceOrder(c echo.Context) error {
-	return c.JSON(200, "tst")
+type OrderType string
+
+const (
+	MarketOrder OrderType = "MARKET"
+	LimitOrder  OrderType = "LIMIT"
+)
+
+type Market string
+
+const (
+	MarketETH Market = "ETH"
+)
+
+type Exchange struct {
+	orderbooks map[Market]*orderbook.Orderbook
+}
+
+func NewExchange() *Exchange {
+	orderbooks := make(map[Market]*orderbook.Orderbook)
+	orderbooks[MarketETH] = orderbook.NewOrderbook()
+
+	return &Exchange{
+		orderbooks: orderbooks,
+	}
+}
+
+type PlaceOrderRequest struct {
+	Type   OrderType // limit or market
+	Bid    bool
+	Size   float64
+	Price  float64
+	Market Market
+}
+
+func (ex *Exchange) handlePlaceOrder(c echo.Context) error {
+	var placeOrderData PlaceOrderRequest
+
+	if err := json.NewDecoder(c.Request().Body).Decode(&placeOrderData); err != nil {
+		return err
+	}
+
+	market := Market(placeOrderData.Market)
+	ob := ex.orderbooks[market]
+	order := orderbook.NewOrder(placeOrderData.Bid, placeOrderData.Size)
+
+	ob.PlaceLimitOrder(placeOrderData.Price, order)
+
+	return c.JSON(200, map[string]any{"msg": "order placed"})
 }
