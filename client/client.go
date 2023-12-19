a67db2c7cdc0bd09837d3235987849f743100061
Alex Ira <27619740+4lexir4@users.noreply.github.com>
2023-12-19 16:53:25 +1100

    place limit order params

--- client/client.go
+++ client/client.go
@@ -1,6 +1,15 @@
 package client
 
-import "net/http"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/4lexir4/cx/server"
+)
+
+const Endpoint = "http://localhost:3000"
 
 type Client struct {
 	*http.Client
@@ -10,4 +19,42 @@ func NewClient() *Client {
 	return &Client{
 		Client: http.DefaultClient,
 	}
+}
+
+type PlaceLimitOrderParams struct {
+	UserID int64
+	Bid    bool
+	Price  float64
+	Size   float64
+}
+
+func (c *Client) PlaceLimitOrder(p *PlaceLimitOrderParams) error {
+	params := &server.PlaceOrderRequest{
+		UserID: p.UserID,
+		Type:   server.LimitOrder,
+		Bid:    p.Bid,
+		Size:   p.Size,
+		Price:  p.Price,
+		Market: server.MarketETH,
+	}
+
+	body, err := json.Marshal(params)
+	if err != nil {
+		return err
+	}
+
+	e := Endpoint + "/order"
+	req, err := http.NewRequest(http.MethodPost, e, bytes.NewReader(body))
+	if err != nil {
+		return err
+	}
+
+	resp, err := c.Do(req)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%+v", resp)
+
+	return nil
 }
--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/4lexir4/cx/server"
+import (
+	"time"
+
+	"github.com/4lexir4/cx/client"
+	"github.com/4lexir4/cx/server"
+)
 
 func main() {
-	server.StartServer()
+	go server.StartServer()
+
+	time.Sleep(1 * time.Second)
+
+	c := client.NewClient()
+
+	params := &client.PlaceLimitOrderParams{
+		UserID: 8,
+		Bid:    true,
+		Price:  4000.0,
+		Size:   4000.0,
+	}
+
+	if err := c.PlaceLimitOrder(params); err != nil {
+		panic(err)
+	}
 }
