64b6e7a680937b4b4cee5198ac61963b435c60bd
Alex Ira <27619740+4lexir4@users.noreply.github.com>
2024-01-04 13:00:26 +1100

    seeding market

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/4lexir4/cx/server"
 )
 
-const ethPrice = 1_281
+const ethPrice = 1_281.0
 
 func main() {
 	go server.StartServer()
@@ -20,6 +20,34 @@ func main() {
 	go makeMarketSimple(c)
 
 	select {}
+}
+
+func seedMarket(c *client.Client) {
+	// NOTE this should be an async call to fetch the price
+	currentPrice := ethPrice
+
+	priceOffset := 100.0
+
+	bidOrder := client.PlaceOrderParams{
+		UserID: 8,
+		Bid:    true,
+		Price:  currentPrice - priceOffset,
+		Size:   10,
+	}
+	_, err := c.PlaceLimitOrder(&bidOrder)
+	if err != nil {
+		panic(err)
+	}
+	askOrder := client.PlaceOrderParams{
+		UserID: 8,
+		Bid:    false,
+		Price:  currentPrice + priceOffset,
+		Size:   10,
+	}
+	_, err = c.PlaceLimitOrder(&askOrder)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func makeMarketSimple(c *client.Client) {
@@ -34,6 +62,11 @@ func makeMarketSimple(c *client.Client) {
 		bestBid, err := c.GetBestBid()
 		if err != nil {
 			panic(err)
+		}
+
+		if bestAsk == 0 && bestBid == 0 {
+			seedMarket(c)
+			continue
 		}
 
 		fmt.Println("best bid", bestBid)
